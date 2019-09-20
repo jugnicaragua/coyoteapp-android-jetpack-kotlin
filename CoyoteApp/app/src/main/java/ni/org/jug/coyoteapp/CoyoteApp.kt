b74336f83346e7d462143b7a43137eb474be49e8
Luis Alberto Guido Calderón <luchonetcc@gmail.com>
2019-09-19 23:32:02 -0600

    Agregue la clase Application llamada CoyoteApp
    En este caso se creoo esta clase para poder inicializar Crashlytics.

---
+++ CoyoteApp/app/src/main/java/ni/org/jug/coyoteapp/CoyoteApp.kt
@@ -0,0 +1,14 @@
+package ni.org.jug.coyoteapp
+
+import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
+
+class CoyoteApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        // Obtain the FirebaseAnalytics instance.
+        FirebaseAnalytics.getInstance(this)
+    }
+
+}
